2d6555b11f2541c6622cf5f2783b5c8a5da1d57a
Todd Stuke <tddstuke@gmail.com>
2022-05-09 20:44:32 -0500

    created promises on separate page

--- app.js
+++ app.js
@@ -1,52 +1,7 @@
 const { profile } = require("console");
 const inquirer = require("inquirer");
-const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site");
 const generatePage = require("./src/page-template.js");
-const mockData = {
-  name: "Lernantino",
-  github: "lernantino",
-  confirmAbout: true,
-  about:
-    "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.",
-  projects: [
-    {
-      name: "Run Buddy",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
-      languages: ["HTML", "CSS"],
-      link: "https://github.com/lernantino/run-buddy",
-      feature: true,
-      confirmAddProject: true,
-    },
-    {
-      name: "Taskinator",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
-      languages: ["JavaScript", "HTML", "CSS"],
-      link: "https://github.com/lernantino/taskinator",
-      feature: true,
-      confirmAddProject: true,
-    },
-    {
-      name: "Taskmaster Pro",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
-      languages: ["JavaScript", "jQuery", "CSS", "HTML", "Bootstrap"],
-      link: "https://github.com/lernantino/taskmaster-pro",
-      feature: false,
-      confirmAddProject: true,
-    },
-    {
-      name: "Robot Gladiators",
-      description:
-        "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.",
-      languages: ["JavaScript"],
-      link: "https://github.com/lernantino/robot-gladiators",
-      feature: false,
-      confirmAddProject: false,
-    },
-  ],
-};
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -183,28 +138,25 @@ const promptProject = (portfolioData) => {
       }
     });
 };
-// promptUser()
-//   .then(promptProject)
-//   .then((portfolioData) => {
-const pageHTML = generatePage(mockData);
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    return generatePage(portfolioData);
+  })
 
-fs.writeFile("./dist/index.html", pageHTML, (err) => {
-  if (err) throw new Error(err);
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
 
-  console.log(
-    "Page created! Check out index.html in this directory to see it!"
-  );
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
 
-  fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
-    if (err) throw new Error(err);
-    console.log("Style sheet copied successfully!");
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+
+  .catch((err) => {
+    console.log(err);
   });
-});
-//   });
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-//   console.log("Portfolio complete! Checkout index.html to see the output!");
-// });
